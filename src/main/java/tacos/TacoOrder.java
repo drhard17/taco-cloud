16dd5604892476fb719a72b92203cb8d7d62925c
drhard17 <maslak84@gmail.com>
2024-12-17 20:49:26 +0300

    jdbcTemplate repo

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -1,17 +1,22 @@
 package tacos;
-import java.util.List;
 
-import org.hibernate.validator.constraints.CreditCardNumber;
+import java.util.List;
+import java.util.Date;
 
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import lombok.Data;
 
 @Data
-public class TacoOrder {
+public class TacoOrder implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private long id;
+    private Date placedAt;
 
     @NotBlank(message = "Delivery name is required")
     private String deliveryName;
@@ -21,17 +26,18 @@ public class TacoOrder {
 
     @NotBlank(message = "City is required")
     private String deliveryCity;
-    
+
     @NotBlank(message = "State is required")
     private String deliveryState;
-    
+
     @NotBlank(message = "Zip code is required")
     private String deliveryZip;
 
-    @CreditCardNumber(message = "Invalid CC number")    
+    @Pattern(regexp = "^[0-9]{16}$", 
+            message =  "Any CC-like number is required")
     private String ccNumber;
 
-    @Pattern(regexp = "(0[0-9]1[0-2])([\\/])([2-9][0-9])")
+    @Pattern(regexp = "[0-9]{2}/[0-9]{2}", message = "Date as MM/YY is required")
     private String ccExpiration;
 
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,8 +1,8 @@
 package tacos.web;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -17,8 +17,8 @@ import lombok.extern.slf4j.Slf4j;
 import tacos.TacoOrder;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
+import tacos.data.IngredientRepository;
 import tacos.Taco;
-
 
 @Slf4j
 @Controller
@@ -26,28 +26,24 @@ import tacos.Taco;
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
 
+    private final IngredientRepository ingredientRepo;
+
+    public DesignTacoController(IngredientRepository ingredientRepo) {
+        this.ingredientRepo = ingredientRepo;
+    }
+
     @ModelAttribute
     public void addIngredientsToModel(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-            new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-            new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-            new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-            new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-            new Ingredient("TMTO", "Diced Totamoes", Type.VEGGIES),
-            new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-            new Ingredient("CHED", "Chedder", Type.CHEESE),
-            new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-            new Ingredient("SLSA", "Salsa", Type.SAUCE),
-            new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-        );
+
+        Iterable<Ingredient> ingredients = ingredientRepo.findAll();
 
         Type[] types = Ingredient.Type.values();
         for (Type type : types) {
             model.addAttribute(type.toString().toLowerCase(),
-            filterByType(ingredients, type));
+                    filterByType(ingredients, type));
         }
     }
-    
+
     @ModelAttribute(name = "tacoOrder")
     public TacoOrder order() {
         return new TacoOrder();
@@ -73,15 +69,14 @@ public class DesignTacoController {
 
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
-        
+
         return "redirect:/orders/current";
     }
-    
+
     private Iterable<Ingredient> filterByType(
-        List<Ingredient> ingredients, Type type) {
-            return ingredients
-                    .stream()
-                    .filter(x -> x.getType().equals(type))
-                    .collect(Collectors.toList());
-        }
+            Iterable<Ingredient> ingredients, Type type) {
+        return StreamSupport.stream(ingredients.spliterator(), false)
+                .filter(i -> i.getType().equals(type))
+                .collect(Collectors.toList());
+    }
 }

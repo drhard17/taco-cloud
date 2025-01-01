c14438f6e8245e427566e21c37f8031345d3b626
drhard17 <maslak84@gmail.com>
2025-01-01 16:52:49 +0300

    hardcoded users

---
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -0,0 +1,53 @@
+package tacos.security;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+// import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+// import tacos.data.UserRepository;
+// import tacos.User;
+
+@Configuration
+public class SecurityConfig {
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        List<UserDetails> userList = new ArrayList<>();
+        userList.add(new User(
+                "buzz", encoder.encode("password"),
+                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+        userList.add(new User(
+                "woody", encoder.encode("password"),
+                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+        return new InMemoryUserDetailsManager(userList);
+    }
+
+    // @Bean
+    // public UserDetailsService userDetailsService(UserRepository userRepo) {
+    //     return username -> {
+    //         User user = userRepo.findByUsername(username);
+    //         if (user != null) {
+    //             return user;
+    //         }
+    //         throw new UsernameNotFoundException(
+    //                 "User '" + username + "' not found");
+    //     };
+    // }
+
+}
